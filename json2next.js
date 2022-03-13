5ddb766f8f7eac43adf9325736716457d3a33d4c
Jannis Baum <jannis.baum@gmail.com>
2022-03-13 11:29:14 +0100

    create react / next components from json

---
+++ json2next.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+const AllComponents = { Image, Link }
+
+export default function json2react(jsonComp) {
+    return React.createElement(
+        AllComponents[jsonComp.name] ?? jsonComp.name,
+        jsonComp.props,
+        jsonComp.children ? jsonComp.children.map(child => json2react(child)) : undefined);
+}
+
