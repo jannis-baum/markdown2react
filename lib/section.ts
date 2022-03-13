13ed418e45057713fef061c4585d87b06554ef0c
Jannis Baum <jannis.baum@gmail.com>
2022-03-13 20:29:16 +0100

    option to specify generic props instead of just class name

--- lib/section.ts
+++ lib/section.ts
@@ -1,5 +1,5 @@
 import { ComponentDef } from "../markdown2json";
-import { cls_section, ele_section } from "../definitions";
+import { prp_section, ele_section } from "../definitions";
 
 export default function section(paras: Array<ComponentDef>): ComponentDef {
     return _section(paras, 'section-body');
@@ -25,7 +25,7 @@ function _section(innerParas: Array<ComponentDef>, key: string, depth: number = 
 
     return {
         name: ele_section,
-        props: { key: key, className: cls_section(depth) },
+        props: { key: key, ...prp_section(depth) },
         children: sectionParas
     }
 }
