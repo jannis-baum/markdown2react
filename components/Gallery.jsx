c9efeaba4dcccb1275dc5b01e39a2b771030484c
Jannis Baum <jannis.baum@gmail.com>
2022-04-03 22:31:51 +0200

    gallery clickable

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Gallery.module.scss';
 
 const Gallery = (props) => {
-    return (<div className={`${styles.gallery} ${styles.cols}`}>
-        {React.Children.map(props.children, (child) => 
-            React.cloneElement(child, { className: `${child.props.className} ${styles.galleryItem}` })
-        )}
-    </div>);
+    const [bigImageIdx, setBigImageIdx] = useState(null);
+    const children = React.Children.toArray(props.children);
+    return (<>
+        {bigImageIdx !== null && <div onClick={ () => setBigImageIdx(null) } className={styles.bigImageContainer}>
+            <div onClick={(e) => { e.stopPropagation() }}>
+                {React.cloneElement(children[bigImageIdx], { className: styles.bigImage })}
+            </div>
+        </div>}
+
+        <div className={`${styles.gallery} ${styles.cols}`}>
+            {children.map((child, i) => (
+                <div key={child.key} onClick={() => { setBigImageIdx(i) }}>
+                    {React.cloneElement(child, { className: styles.galleryItem })}
+                </div>
+            ))}
+        </div>
+    </>);
 }
 
 export default Gallery;
